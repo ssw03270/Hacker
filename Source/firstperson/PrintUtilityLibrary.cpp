d1033d03f089514dfd549f6b007484c2fe8168b6
SooBin Lim <50435598+dnpcs12@users.noreply.github.com>
2022-01-25 19:58:45 +0900

    Revert "printstring"
    This reverts commit e72efa1293a478c3d1a0489ed1b3461892dd91c4.

--- Source/firstperson/PrintUtilityLibrary.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "PrintUtilityLibrary.h"
-
-bool UPrintUtilityLibrary::FileSaveString(FString SaveText, FString FileName)
-{
-	FFileHelper::SaveStringToFile(SaveText,*(FPaths::GameAgnosticSavedDir() + FileName));
-	return false;
-}
